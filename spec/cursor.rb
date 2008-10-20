7a097ddcb6605339e6d77aad369778a9c990f0e5
Michael Fellinger <m.fellinger@gmail.com>
2008-10-20 09:51:11 +0900

    Fix cursor spec

--- spec/cursor.rb
+++ spec/cursor.rb
@@ -3,65 +3,19 @@ require 'lib/ver/buffer'
 require 'lib/ver/buffer/memory'
 require 'bacon'
 
-# Bacon.extend(Bacon::TestUnitOutput)
 Bacon.summary_on_exit
-
-=begin
-$buffer_content = <<BUFFER.strip
-This is a little example.
-We will attempt to provide a large variety of buffer strutures in this one.
-
-Like empty lines, or long lines.
-Even lines
-differing in length vastly, as to achive a better feeling of what we can do with it and how to improve.
-BUFFER
-=end
-
-$buffer_content = <<EXAMPLE.strip
-require 'lib/ver/cursor'
-require 'lib/ver/buffer'
-require 'lib/ver/buffer/memory'
-require 'bacon'
-
-# Bacon.extend(Bacon::TestUnitOutput)
-Bacon.summary_on_exit
-
-=begin
-EXAMPLE
+SPEC_ROOT = File.dirname(__FILE__)
 
 describe 'Cursor' do
   def new_buffer
-    VER::MemoryBuffer.new(:test, $buffer_content.dup)
+    @content ||= File.read(File.join(SPEC_ROOT, 'content/lines.txt'))
+    VER::MemoryBuffer.new(:test, @content)
   end
 
   def cursor(pos)
     cursor = VER::Cursor.new(new_buffer, pos)
   end
 
-  move, start = :up, 150
-
-  should "debug #{move}" do
-    buffer = new_buffer
-    cursor = VER::Cursor.new(buffer, start)
-    old = start
-    puts '', buffer[0..-1], ''
-
-    $buffer_content.count("\n").times do |n|
-      puts "= #{n} =".center(80, "=")
-      old = cursor.pos
-      char = buffer[cursor.pos, 1]
-      p :here => char
-      buffer[cursor.pos, 1] = 'X' unless char == "\n"
-      # buffer[cursor.pos, 1] = 'O' if char == "\n"
-      puts '', buffer[0..-1], ''
-      cursor.send(move)
-      break if cursor.pos == old
-    end
-
-    1.should == 1
-  end
-
-=begin
   should 'not go up on first line' do
     cursor = cursor(5)
     cursor.up
@@ -113,26 +67,48 @@ describe 'Cursor' do
   should 'go down indented' do
     cursor = cursor(110)
     cursor.down
-    cursor.pos.should == 144
+    cursor.pos.should == 143
   end
 
   should 'go down to last line' do
     cursor = cursor(139)
     cursor.down
-    cursor.pos.should == 149
+    cursor.pos.should == 150
   end
 
   should 'not go down from last line' do
     cursor = cursor(200)
     cursor.down
-    cursor.pos.should == 200
+    cursor.pos.should == 251
   end
 
   should 'go down several lines' do
     cursor = cursor(106)
     cursor.down
     cursor.down
-    cursor.pos.should == 150
+    cursor.down
+    cursor.pos.should == 251
   end
-=end
+end
+
+__END__
+move, start = :up, 150
+
+content = File.read(File.join(SPEC_ROOT, 'content/lines.txt'))
+buffer = VER::MemoryBuffer.new(:test, content)
+cursor = VER::Cursor.new(buffer, start)
+
+old = start
+puts '', buffer[0..-1], ''
+
+$buffer_content.count("\n").times do |n|
+  puts "= #{n} =".center(80, "=")
+  old = cursor.pos
+  char = buffer[cursor.pos, 1]
+  p :here => char
+  buffer[cursor.pos, 1] = 'X' unless char == "\n"
+  # buffer[cursor.pos, 1] = 'O' if char == "\n"
+  puts '', buffer[0..-1], ''
+  cursor.send(move)
+  break if cursor.pos == old
 end
