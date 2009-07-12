c0c257f425f53754e08a2080477c10394a7732b9
Michael Fellinger <m.fellinger@gmail.com>
2009-07-12 17:03:51 +0900

    All control keys are now captured by VER, ncurses call order matters in the setup

--- lib/ver/ncurses.rb
+++ lib/ver/ncurses.rb
@@ -26,41 +26,6 @@ module VER
     # The default value for keypad is false.
     Ncurses.keypad(stdscr, 1)
 
-    # The nl and nonl routines control whether the underlying display device
-    # translates the return key into newline on input, and whether it
-    # translates newline into return and line-feed on output (in either case,
-    # the call addch('\n') does the equivalent of return and line feed on the
-    # virtual screen).
-    # Initially, these translations do occur.
-    # If you disable them using nonl, curses will be able to make better use of
-    # the line-feed capability, resulting in faster cursor motion.
-    # Also, curses will then be able to detect the return key.
-    Ncurses.nonl
-
-    # The raw and noraw routines place the terminal into or out of raw mode.
-    # Raw mode is similar to cbreak mode, in that characters typed are
-    # immediately passed through to the user program.
-    # The differences are that in raw mode, the interrupt, quit, suspend, and
-    # flow control characters are all passed through uninterpreted, instead of
-    # generating a signal.
-    # The behavior of the BREAK key depends on other bits in the tty driver
-    # that are not set by curses.
-    Ncurses.raw
-
-    # Normally, the tty driver buffers typed characters until a newline or
-    # carriage return is typed.
-    # The cbreak routine disables line buffering and
-    # erase/kill character-processing (interrupt and flow control characters
-    # are unaffected), making characters typed by the user immediately
-    # available to the program.
-    # Ncurses.cbreak
-
-    # The echo and noecho routines control whether characters typed by the user
-    # are echoed by getch as they are typed.
-    # Echoing by the tty driver is always disabled, but initially getch is in
-    # echo mode, so characters typed are echoed.
-    Ncurses.noecho
-
     # The curs_set routine sets the cursor state is set to invisible, normal,
     # or very visible for visibility equal to 0, 1, or 2 respectively.
     # If the terminal supports the visibility requested, the previous cursor
@@ -80,6 +45,41 @@ module VER
     # ready, getch returns ERR. If disabled (bf is FALSE), getch waits until a
     # key is pressed.
     # Ncurses::nodelay(Ncurses::stdscr, 1)
+
+    # The nl and nonl routines control whether the underlying display device
+    # translates the return key into newline on input, and whether it
+    # translates newline into return and line-feed on output (in either case,
+    # the call addch('\n') does the equivalent of return and line feed on the
+    # virtual screen).
+    # Initially, these translations do occur.
+    # If you disable them using nonl, curses will be able to make better use of
+    # the line-feed capability, resulting in faster cursor motion.
+    # Also, curses will then be able to detect the return key.
+    Ncurses.nonl
+
+    # The echo and noecho routines control whether characters typed by the user
+    # are echoed by getch as they are typed.
+    # Echoing by the tty driver is always disabled, but initially getch is in
+    # echo mode, so characters typed are echoed.
+    Ncurses.noecho
+
+    # Normally, the tty driver buffers typed characters until a newline or
+    # carriage return is typed.
+    # The cbreak routine disables line buffering and
+    # erase/kill character-processing (interrupt and flow control characters
+    # are unaffected), making characters typed by the user immediately
+    # available to the program.
+    Ncurses.cbreak
+
+    # The raw and noraw routines place the terminal into or out of raw mode.
+    # Raw mode is similar to cbreak mode, in that characters typed are
+    # immediately passed through to the user program.
+    # The differences are that in raw mode, the interrupt, quit, suspend, and
+    # flow control characters are all passed through uninterpreted, instead of
+    # generating a signal.
+    # The behavior of the BREAK key depends on other bits in the tty driver
+    # that are not set by curses.
+    Ncurses.raw
   end
 
   def stop_ncurses
