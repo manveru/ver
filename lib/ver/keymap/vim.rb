90218f64af06680ca0ae0663490ae1806447d0e7
Michael Fellinger <m.fellinger@gmail.com>
2009-10-07 18:51:20 +0900

    sort keymap definition by method name

--- lib/ver/keymap/vim.rb
+++ lib/ver/keymap/vim.rb
@@ -16,65 +16,65 @@ module VER
 
       vim.add_mode :views do |mode|
         mode.map :view_create,     %w[Control-w plus]
-        mode.map :view_remove,     %w[Control-w plus]
         mode.map :view_focus_next, %w[Control-w j]
         mode.map :view_focus_prev, %w[Control-w k]
+        mode.map :view_remove,     %w[Control-w plus]
       end
 
       vim.add_mode :move do |mode|
-        mode.map :go_line,              %w[g g]
+        mode.map :go_beginning_of_line, %w[0]
         mode.map :go_char_left,         %w[h], %w[Left]
         mode.map :go_char_right,        %w[l], %w[Right]
         mode.map :go_end_of_file,       %w[G]
         mode.map :go_end_of_line,       %w[dollar]
+        mode.map :go_line,              %w[g g]
         mode.map :go_line_down,         %w[j], %w[Down]
         mode.map :go_line_up,           %w[k], %w[Up]
+        mode.map :go_next_newline_block, %w[braceleft]
         mode.map :go_page_down,         %w[Control-f], %w[Next]
         mode.map :go_page_up,           %w[Control-b], %w[Prior]
-        mode.map :go_beginning_of_line, %w[0]
+        mode.map :go_prev_newline_block, %w[braceright]
         mode.map :go_word_left,         %w[b]
         mode.map :go_word_right,        %w[w]
-        mode.map :go_next_newline_block, %w[braceleft]
-        mode.map :go_prev_newline_block, %w[braceright]
       end
 
       vim.add_mode :search do |mode|
-        mode.map :status_search,                 %w[slash]
         mode.map :search_next,                   %w[n]
-        mode.map :search_prev,                   %w[N]
         mode.map :search_next_word_under_cursor, %w[asterisk]
+        mode.map :search_prev,                   %w[N]
         mode.map :search_prev_word_under_cursor, %w[numbersign]
+        mode.map :status_search,                 %w[slash]
       end
 
       vim.add_mode :control do |mode|
         mode.inherits :basic, :move, :views, :search
 
-        mode.map :delete_char_right,             %w[x]
+        mode.map :copy_left_word,                %w[y b]
+        mode.map :copy_line,                     %w[y y], %w[Y]
+        mode.map :copy_right_word,               %w[y w]
         mode.map :delete_char_left,              %w[X]
-        mode.map :delete_word_right,             %w[d w]
-        mode.map :delete_word_left,              %w[d b]
+        mode.map :delete_char_right,             %w[x]
         mode.map :delete_line,                   %w[d d]
         mode.map :delete_to_eol,                 %w[D]
         mode.map :delete_to_eol_then_insert,     %w[C]
-        mode.map :start_replace_mode,            %w[R]
-        mode.map :start_insert_mode,             %w[i]
-        mode.map :start_select_char_mode,        %w[v]
-        mode.map :start_select_line_mode,        %w[V]
-        mode.map :start_select_block_mode,       %w[Control-v]
+        mode.map :delete_word_left,              %w[d b]
+        mode.map :delete_word_right,             %w[d w]
         mode.map :eol_then_insert_mode,          %w[A]
-        mode.map :sol_then_insert_mode,          %w[I]
-        mode.map :smart_evaluate,                %w[Alt-e]
+        mode.map :indent_line,                   %w[greater]
         mode.map :insert_indented_newline_above, %w[O]
         mode.map :insert_indented_newline_below, %w[o]
         mode.map :join_lines,                    %w[J]
-        mode.map :indent_line,                   %w[greater]
-        mode.map :unindent_line,                 %w[less]
-        mode.map :undo,                          %w[u]
-        mode.map :redo,                          %w[Control-r]
-        mode.map :copy_line,                     %w[y y], %w[Y]
-        mode.map :copy_right_word,               %w[y w]
-        mode.map :copy_left_word,                %w[y b]
         mode.map :paste,                         %w[p]
+        mode.map :redo,                          %w[Control-r]
+        mode.map :smart_evaluate,                %w[Alt-e]
+        mode.map :sol_then_insert_mode,          %w[I]
+        mode.map :start_insert_mode,             %w[i]
+        mode.map :start_replace_mode,            %w[R]
+        mode.map :start_select_block_mode,       %w[Control-v]
+        mode.map :start_select_char_mode,        %w[v]
+        mode.map :start_select_line_mode,        %w[V]
+        mode.map :undo,                          %w[u]
+        mode.map :unindent_line,                 %w[less]
 
         mode.missing :ignore_string
       end
@@ -87,21 +87,20 @@ module VER
         mode.inherits :basic, :views
         mode.arguments = false
 
+        mode.map :delete_char_left,        %w[BackSpace]
+        mode.map :delete_char_right,       %w[Delete]
         mode.map :go_char_left,            %w[Left]
         mode.map :go_char_right,           %w[Right]
         mode.map :go_line_down,            %w[Down]
         mode.map :go_line_up,              %w[Up]
         mode.map :go_page_down,            %w[Control-f], %w[Next]
-        mode.map :go_page_up,              %w[Control-b], %w[Prior]
-        mode.map :smart_evaluate,          %w[Alt-e]
-        mode.map :insert_indented_newline, %w[Return]
-        mode.map :delete_char_left,        %w[BackSpace]
-        mode.map :delete_char_right,       %w[Delete]
-
-        mode.map :go_page_up,              %w[Shift-Up]
         mode.map :go_page_down,            %w[Shift-Down]
+        mode.map :go_page_up,              %w[Control-b], %w[Prior]
+        mode.map :go_page_up,              %w[Shift-Up]
         mode.map :go_word_left,            %w[Shift-Left]
         mode.map :go_word_right,           %w[Shift-Right]
+        mode.map :insert_indented_newline, %w[Return]
+        mode.map :smart_evaluate,          %w[Alt-e]
 
         { " "  => "space",
           "!"  => "exclam",
@@ -158,14 +157,13 @@ module VER
 
         mode.map :copy_selection, %w[y], %w[Y]
         mode.map :delete_selection, %w[d], %w[D]
-        mode.map :smart_evaluate, %w[Alt-e]
-        mode.map :pipe_selection, %w[exclam]
         mode.map :indent_selection, %w[greater]
-        mode.map :unindent_selection, %w[less]
-
+        mode.map :pipe_selection, %w[exclam]
+        mode.map :smart_evaluate, %w[Alt-e]
+        mode.map :switch_select_block_mode, %w[Control-v]
         mode.map :switch_select_char_mode,  %w[v]
         mode.map :switch_select_line_mode,  %w[V]
-        mode.map :switch_select_block_mode, %w[Control-v]
+        mode.map :unindent_selection, %w[less]
       end
 
       vim.add_mode :select_char do |mode|
