997b57b7640cce14d7a8dc1a5bd631ecfdfd22bc
Michael Fellinger <m.fellinger@gmail.com>
2009-10-20 18:27:06 +0900

    New console based on EventMachine

--- lib/ver/view/console.rb
+++ lib/ver/view/console.rb
@@ -1,6 +1,31 @@
 module VER
   class View
     class Console
+      class Process < EventMachine::Connection
+        attr_accessor :callback
+
+        def receive_data data
+          callback.on_stdout(data)
+        end
+
+        def unbind
+          callback.destroy
+          EM.stop
+        end
+      end
+
+      class Stderr < EventMachine::Connection
+        attr_accessor :callback
+
+        def initialize(stderr)
+          @stderr = stderr
+        end
+
+        def receive_data(data)
+          callback.on_stderr(data)
+        end
+      end
+
       def initialize(parent)
         @parent = parent
         setup_widgets
@@ -26,9 +51,9 @@ module VER
           foreground: 'white',
           wrap: :word
         )
-        @text.tag_configure :input,  foreground: 'green', background: 'black'
-        @text.tag_configure :output, foreground: 'white', background: 'black'
-        @text.tag_configure :error,  foreground: 'red',   background: 'black'
+        @text.tag_configure :stdin,  foreground: 'green', background: 'black'
+        @text.tag_configure :stdout, foreground: 'white', background: 'black'
+        @text.tag_configure :stderr, foreground: 'red',   background: 'black'
 
         @entry = Tk::Tile::Entry.new(@parent)
         @entry.pack fill: :x, side: :bottom
@@ -38,7 +63,7 @@ module VER
       def setup_events
         @entry.bind('Control-q'){ Tk.exit }
         @entry.bind('Return'){
-          @buffer << @entry.value
+          send_data @entry.value
           @entry.delete 0, :end
         }
       end
@@ -64,67 +89,55 @@ module VER
           @buffer << 'echo $BASH $BASH_VERSION'
         end
 
-        @thread = Thread.new{
-          Open3.popen3(*opts) do |write, read, error, popen_thread|
-            catch(:stop) do
-              loop do
-                # that's where the PID of the console hides
-                # p Hash[popen_thread.keys.map{|k| [k, popen_thread[k]] }]
-                step(write, read, error)
-                sleep 0.1
-              end
-            end
-
-            destroy
-          end
-        }
-      end
-
-      def step(write, read, error)
-        r, w, e = IO.select([read], [write], [error], 1)
-
-        if w && w.size > 0
-          while line = @buffer.shift
-            on_stdin line
-            w.map{|io| io.puts(line) }
-          end
-        end
-
-        read_nonblock(e){|line| on_stderr(line) }
-        read_nonblock(r){|line| on_stdout(line) }
-
-      rescue Errno::EPIPE, EOFError
-        throw :stop
-      rescue Exception => ex
-        VER.error(ex)
-        throw :stop
-      end
-
-      def read_nonblock(ios)
-        if ios && ios.size > 0
+        # FIXME: this should have proper shell escapes
+        popen3(opts.join(' '), self) do |stdin|
           begin
-            results = ios.map{|io| io.read_nonblock(2 << 15) }
-            results.each do |result|
-              result.each_line do |line|
-                yield line
-              end
+            @stdin = stdin
+
+            while line = @buffer.shift
+              send_data(line)
             end
-          rescue IO::WaitReadable, Errno::EINTR => ex
-            # the IO isn't ready yet, just go back to work
+          rescue => ex
+            p ex
           end
         end
+      end
+
+      # the callback should have #on_stdout and #on_stderr
+      # The method yields the stdin and you can use #send_data on it.
+      # This seems to have the side-effect of messing with the original $stderr,
+      # maybe there are other solutions.
+      def popen3(cmd, callback)
+        old_stderr = $stderr.dup
+        rd, wr = IO::pipe
+        $stderr.reopen(wr)
+
+        popen_stdin = EM.popen(cmd, Process)
+        popen_stdin.callback = callback
+
+        popen_stderr = EM.attach(rd, Stderr, rd)
+        popen_stderr.callback = callback
+
+        yield(popen_stdin) if block_given?
+
+        $stderr.reopen old_stderr
+      end
+
+      def send_data(string)
+        @stdin.send_data("#{string}\n")
+        on_stdin(string)
       end
 
       def on_stdin(string)
-        output "i> #{string}\n", :input # bg: 'black', fg: 'green'
+        output "i> #{string}\n", :stdin
       end
 
       def on_stdout(string)
-        output "#{string}", :output, # bg: 'black', fg: 'white'
+        output "#{string}", :stdout
       end
 
       def on_stderr(string)
-        output "e> #{string}", :error # bg: 'black', fg: 'red'
+        output "e> #{string}", :stderr
       end
 
       def output(string, tag)
