1d666190880e4d49cef9bcbddd8e3e6785463548
Michael Fellinger <m.fellinger@gmail.com>
2009-10-20 18:04:41 +0900

    Better searching, escape term if it's not a valid regular expression, use nice colors

--- lib/ver/methods/search.rb
+++ lib/ver/methods/search.rb
@@ -1,9 +1,20 @@
 module VER
   module Methods
     module Search
+      SEARCH_HIGHLIGHT = {
+        foreground: '#000',
+        background: '#ff0',
+      }
+
       def status_search
         status_ask 'Search term: ' do |term|
-          tag_all_matching(:search, Regexp.new(term))
+          begin
+            needle = Regexp.new(term)
+          rescue RegexpError
+            needle = Regexp.escape(term)
+          end
+
+          tag_all_matching(:search, needle, SEARCH_HIGHLIGHT)
         end
       end
 
@@ -19,13 +30,13 @@ module VER
 
       def search_next_word_under_cursor
         word = get('insert wordstart', 'insert wordend')
-        tag_all_matching(:search, word)
+        tag_all_matching(:search, word, SEARCH_HIGHLIGHT)
         search_next
       end
 
       def search_prev_word_under_cursor
         word = get('insert wordstart', 'insert wordend')
-        tag_all_matching(:search, word)
+        tag_all_matching(:search, word, SEARCH_HIGHLIGHT)
         search_prev
       end
     end
