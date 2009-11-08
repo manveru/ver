414ca9d8635d20b7b85838fa6f0ce1b087759cf3
Michael Fellinger <m.fellinger@gmail.com>
2009-11-08 18:27:55 +0900

    Fix tooltip

--- lib/ver/tooltip.rb
+++ lib/ver/tooltip.rb
@@ -4,10 +4,10 @@ module Tk
     def initialize(on_widget, text)
       super
 
-      widget.bind('Any-Enter'   ){ Tk.after(500){ show    }}
-      widget.bind('Any-Leave'   ){ Tk.after(500){ destroy }}
-      widget.bind('Any-KeyPress'){ Tk.after(500){ destroy }}
-      widget.bind('Any-Button'  ){ Tk.after(500){ destroy }}
+      widget.bind('<Any-Enter>'   ){ Tk::After.ms(500){ show    }}
+      widget.bind('<Any-Leave>'   ){ Tk::After.ms(500){ destroy }}
+      widget.bind('<Any-KeyPress>'){ Tk::After.ms(500){ destroy }}
+      widget.bind('<Any-Button>'  ){ Tk::After.ms(500){ destroy }}
     end
 
     def destroy
@@ -18,17 +18,16 @@ module Tk
     def show
       root = Tk.root
       root_pointerx, root_pointery = root.winfo_pointerx, root.winfo_pointery
-      return unless TkWinfo.containing(root_pointerx, root_pointery)
+      return unless Tk::Winfo.containing(root_pointerx, root_pointery)
 
       destroy
 
       scrh, scrw = widget.winfo_screenheight, widget.winfo_screenwidth
 
-      self.tooltip = TkToplevel.new(widget, bd: true, bg: 'black'){
-        geometry "+#{scrh}+#{scrw}"
-        overrideredirect true
-        attributes(topmost: true) if Tk::PLATFORM['platform'] == 'windows'
-      }
+      self.tooltip = tooltip = Tk::Toplevel.new(widget, bd: true, bg: 'black')
+      tooltip.wm_geometry = "+#{scrh}+#{scrw}"
+      tooltip.wm_overrideredirect = true
+      tooltip.attributes(topmost: true) if tooltip.tk_windowingsystem == :win32
 
       label = Tk::Label.new(tooltip, bg: :lightyellow, fg: :black,
                                      text: text, justify: :left).pack
@@ -42,7 +41,7 @@ module Tk
         position_x -= ((position_x + width) - root_screen_width)
       end
 
-      tooltip.geometry("#{width}x#{height}+#{position_x}+#{position_y}")
+      tooltip.wm_geometry = "#{width}x#{height}+#{position_x}+#{position_y}"
       tooltip.raise
 
       tooltip.bind('Any-Enter'){ destroy }
@@ -51,9 +50,9 @@ module Tk
   end
 end
 
-require 'tk'
+require 'ffi-tk'
 
-button = TkButton.new(text: 'hello').pack
+button = Tk::Button.new(text: 'hello').pack
 Tk::Tooltip.new(button, 'Hello, World!')
 
 Tk.mainloop
