e4ad3c52df7baf29c1f42988902f6e8bad614ec6
Michael Fellinger <m.fellinger@gmail.com>
2009-11-21 20:01:59 +0900

    Don't search for ' ' on *

--- lib/ver/methods/search.rb
+++ lib/ver/methods/search.rb
@@ -43,12 +43,14 @@ module VER
 
       def search_next_word_under_cursor
         word = get('insert wordstart', 'insert wordend')
+        return if word.squeeze == ' ' # we don't want to match space
         tag_all_matching(:search, word, SEARCH_HIGHLIGHT)
         search_next
       end
 
       def search_prev_word_under_cursor
         word = get('insert wordstart', 'insert wordend')
+        return if word.squeeze == ' ' # we don't want to match space
         tag_all_matching(:search, word, SEARCH_HIGHLIGHT)
         search_prev
       end
