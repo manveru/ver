d63300f4131b344499d0cbf806b50420d19d913f
Julian Langschaedel <meta.rb@gmail.com>
2009-11-24 11:57:36 +0100

    Add view movement shortcuts

--- lib/ver/methods/shortcuts.rb
+++ lib/ver/methods/shortcuts.rb
@@ -9,6 +9,9 @@ module VER
       alias r insert_file_contents
       alias e open_path
       alias o view_find_or_create
+      alias bn view_focus_next
+      alias bp view_focus_prev
+      alias bd view_close
 
       def wq
         w and q
