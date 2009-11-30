9ada1012ea0f542954036bcca34720f211f06899
Michael Fellinger <m.fellinger@gmail.com>
2009-11-30 16:49:12 +0900

    Map some more stuff in diakonos keymap

--- config/keymap/diakonos.rb
+++ config/keymap/diakonos.rb
@@ -47,20 +47,8 @@ module VER
         mode.missing :insert_string
       end
 
-      diakonos.add_mode :buffer do |mode|
-        mode.inherits :basic, :readline
+      diakonos.add_mode :bookmark do |mode|
         mode.arguments = false
-
-        mode.map :backward_char,      %w[Left]
-        mode.map :forward_char,       %w[Right]
-        mode.map :previous_line,      %w[Up]
-        mode.map :next_line,          %w[Down]
-        mode.map :beginning_of_line,  %w[Home]
-        mode.map :end_of_line,        %w[End]
-        mode.map :page_up,            %w[Prior]
-        mode.map :page_down,          %w[Next]
-        mode.map :end_of_file,        %w[Alt-greater]
-        mode.map :go_line,            %w[Alt-less]
 
         mode.map :bookmark_toggle, %w[Alt-b Alt-b]
         mode.map :next_bookmark,   %w[Alt-b Alt-n]
@@ -82,13 +70,54 @@ module VER
         mode.map [:named_bookmark_visit, '3'], %w[Alt-b Alt-3]
         mode.map [:named_bookmark_visit, '4'], %w[Alt-b Alt-4]
         mode.map [:named_bookmark_visit, '5'], %w[Alt-b Alt-5]
+      end
+
+      diakonos.add_mode :ctags do |mode|
+        mode.arguments = false
 
         mode.map :ctags_go,           %w[Alt-t]
         mode.map :ctags_find_current, %w[Alt-parenright]
         mode.map :ctags_prev,         %w[Alt-parenleft]
+      end
+
+      diakonos.add_mode :search do |mode|
+        mode.arguments = false
+
+        mode.map :status_search_next, %w[Control-f]
+        mode.map :search_next,        %w[F3]
+
+        # TODO: this doesn't work, investiate.
+        mode.map :search_prev,        %w[Shift-F3]
+      end
+
+      diakonos.add_mode :buffer do |mode|
+        mode.inherits :basic, :readline, :bookmark, :search
+        mode.arguments = false
+
+        mode.map :backward_char,      %w[Left]
+        mode.map :forward_char,       %w[Right]
+        mode.map :previous_line,      %w[Up]
+        mode.map :next_line,          %w[Down]
+        mode.map :beginning_of_line,  %w[Home]
+        mode.map :end_of_line,        %w[End]
+        mode.map :page_up,            %w[Prior]
+        mode.map :page_down,          %w[Next]
+        mode.map :end_of_file,        %w[Alt-greater]
+        mode.map :go_line,            %w[Alt-less]
 
         mode.map :forward_scroll,  %w[Alt-n]
         mode.map :backward_scroll, %w[Alt-p]
+
+        mode.map [:delete_motion, :backward_char], %w[BackSpace]
+        mode.map [:delete_motion, :forward_char],  %w[Delete]
+        mode.map :kill_line,                       %w[Control-k], %w[Control-d Control-d]
+        mode.map [:delete_motion, :end_of_line],   %w[Control-Alt-k], %w[Control-d dollar]
+        mode.map [:delete_motion, :end_of_line],   %w[Control-Alt-k], %w[Control-d dollar]
+
+        mode.map :insert_indented_newline, %w[Return]
+
+        mode.map :indent_line,   %w[Alt-i], %w[Escape i]
+        mode.map :unindent_line, %w[Escape I]
 =begin
         # TODO
         mode.map :top_of_view,        %w[Alt-comma]
@@ -98,14 +127,7 @@ module VER
 
         mode.map :ask_go_line,        %w[Control-g]
 
-        mode.map [:delete_motion, :backward_char], %w[BackSpace]
-        mode.map [:delete_motion, :forward_char], %w[Delete]
-        mode.map :kill_line, %w[Control-k], %w[Control-d Control-d]
-        mode.map [:delete_motion, :end_of_line], %w[Control-Alt-k], %w[Control-d dollar]
-        mode.map [:delete_motion, :end_of_line], %w[Control-Alt-k], %w[Control-d dollar]
-        mode.map :insert_indented_newline, %w[Return]
-        mode.map :indent_line, %w[Alt-i], %w[Escape i]
-        mode.map :unindent_line, %w[Escape I]
+
         mode.map :forward_join_lines, %w[Alt-j]
         mode.map :backward_join_lines, %w[Escape J]
 =end
