1d3e24a62fffac76e1c1681e98cc144ceca072a5
Michael Fellinger <m.fellinger@gmail.com>
2009-12-04 17:46:55 +0900

    First draft of how specs could be done

---
+++ spec/helper.rb
@@ -0,0 +1,69 @@
+require 'bacon'
+Bacon.summary_at_exit
+
+require 'pathname'
+
+# annoying fixes
+class Pathname
+  alias / join
+
+  def cp(dest)
+    FileUtils.copy_file(expand_path.to_s, dest.to_s, preserve = true)
+  end
+
+  def =~(regexp)
+    to_s =~ regexp
+  end
+end
+
+require 'lib/ver'
+
+VER.run fork: false do
+  Tk::After.idle do
+    describe 'startup' do
+      text = VER.layout.views.first.text
+
+      should 'start with welcome buffer' do
+        text.filename.to_s.should.end_with '.config/ver/welcome'
+      end
+
+      should 'be at start of buffer' do
+        text.index(:insert).to_s.should == "1.0"
+      end
+    end
+  end
+
+  Tk::After.idle do
+    describe VER::Methods::Move do
+      text = VER.layout.views.first.text
+
+      moving = lambda{|from, to, *args|
+        $DEBUG = true
+        text.mark_set(:insert, from)
+        text.send(*args)
+        text.index(:insert).to_s.should == to
+        $DEBUG = false
+      }
+
+      should 'go a char forward' do
+        moving['1.0', '1.1', :forward_char]
+        moving['1.1', '1.2', :forward_char]
+      end
+
+      should 'go a char backward' do
+        moving['1.2', '1.1', :backward_char]
+        moving['1.1', '1.0', :backward_char]
+      end
+
+      should 'go to the beginning of a lines' do
+        moving['2.20', '2.0', :beginning_of_line]
+      end
+
+      should 'go to the end of a line' do
+        moving['2.0', '2.52', :end_of_line]
+      end
+    end
+
+    EM.stop
+  end
+end
