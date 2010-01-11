47b6f613bf9246e454021f853af7c42470f31c94
Michael Fellinger <m.fellinger@gmail.com>
2010-01-11 20:14:15 +0900

    Reduce time between syntax highlights to 100ms

--- lib/ver/text.rb
+++ lib/ver/text.rb
@@ -246,7 +246,7 @@ module VER
       if @syntax = Syntax.from_filename(filename)
         VER.cancel_block(@highlight_block)
 
-        @highlight_block = VER.when_inactive_for(500){
+        @highlight_block = VER.when_inactive_for(100){
           handle_pending_syntax_highlights
         }
 
