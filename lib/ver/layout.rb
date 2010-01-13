d11b80512f9862dfabb8a0807b3d8245710b3017
Michael Fellinger <m.fellinger@gmail.com>
2010-01-14 02:31:58 +0900

    Adding some fun by animating window movement

--- lib/ver/layout.rb
+++ lib/ver/layout.rb
@@ -136,6 +136,65 @@ module VER
       def apply_hidden(windows)
         windows.each(&:place_forget)
       end
+
+      def evolve(window, target)
+        original = window.place_configure
+
+        if original.is_a?(Hash)
+          o_relh, o_relw, o_relx, o_rely = original.values_at(:relheight, :relwidth, :relx, :rely)
+        else
+          o_relh, o_relw, o_relx, o_rely = 0.0, 0.5, 0.5, 1.0
+          # return window.place(target)
+        end
+
+        t_relh, t_relw, t_relx, t_rely = target.values_at(:relheight, :relwidth, :relx, :rely)
+
+        steps = 50
+        stepper = lambda{|from, to|
+          from, to = (from * 1000).to_i, (to * 1000).to_i
+
+          if from > to
+            to.step(from, steps).to_a.reverse
+          elsif to > from
+            from.step(to, steps).to_a
+          else
+            [from]
+          end
+        }
+
+        padder = lambda{|given, max|
+          unless given.empty?
+            ar = Array.new(max){ given.last }
+            ar[0, given.size] = given
+            ar.map{|e| e / 1000.0 }
+          end
+        }
+
+        s_relh = stepper[o_relh, t_relh]
+        s_relw = stepper[o_relw, t_relw]
+        s_relx = stepper[o_relx, t_relx]
+        s_rely = stepper[o_rely, t_rely]
+
+        max = [s_relh, s_relw, s_relx, s_rely].max_by{|a| a.size }.size
+
+        a_relh = padder[s_relh, max] || Array.new(max){ t_relh }
+        a_relw = padder[s_relw, max] || Array.new(max){ t_relw }
+        a_relx = padder[s_relx, max] || Array.new(max){ t_relx }
+        a_rely = padder[s_rely, max] || Array.new(max){ t_rely }
+
+        zipped = a_relh.zip(a_relw.zip(a_relx.zip(a_rely))).map{|a| a.flatten }
+
+        time_per_step = (500 / zipped.size)
+        zipped.each_with_index do |(h, w, x, y), i|
+          Tk::After.ms(i * time_per_step) do
+            window.place(relx: x, rely: y, relheight: h, relwidth: w)
+          end
+        end
+
+        Tk::After.ms((zipped.size + 1) * time_per_step) do
+          window.place(target)
+        end
+      end
     end
 
     module VerticalTiling
@@ -144,17 +203,19 @@ module VER
       module_function
 
       def apply_masters(windows, center, step = 1.0 / windows.size)
-        windows.each_with_index{|window, idx|
-          window.place(relx: 0.0, rely: (step * idx),
-                      relheight: step, relwidth: center) }
+        windows.each_with_index do |window, idx|
+          evolve(window, relx: 0.0, rely: (step * idx),
+                 relheight: step, relwidth: center)
+        end
       end
 
       def apply_stacked(windows, center, step = 1.0 / windows.size)
         relwidth = 1.0 - center
 
-        windows.each_with_index{|window, idx|
-          window.place(relx: center, rely: (step * idx),
-                      relheight: step, relwidth: relwidth) }
+        windows.each_with_index do |window, idx|
+          evolve(window, relx: center, rely: (step * idx),
+                 relheight: step, relwidth: relwidth)
+        end
       end
     end
 
@@ -165,16 +226,17 @@ module VER
 
       def apply_masters(windows, center, step = 1.0 / windows.size)
         windows.each_with_index do |window, idx|
-          window.place(relx: (step * idx), rely: 0.0,
-                       relheight: center, relwidth: step)
+          evolve(window, relx: (step * idx), rely: 0.0,
+                 relheight: center, relwidth: step)
         end
       end
 
       def apply_stacked(windows, center, step = 1.0 / windows.size)
         relheight = 1.0 - center
+
         windows.each_with_index do |window, idx|
-          window.place(relx: (step * idx), rely: center,
-                       relheight: relheight, relwidth: step)
+          evolve(window, relx: (step * idx), rely: center,
+                 relheight: relheight, relwidth: step)
         end
       end
     end
