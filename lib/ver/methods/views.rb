f44b048e3371875f52df9b29a255d24cc4525880
Michael Fellinger <m.fellinger@gmail.com>
2010-01-18 18:52:25 +0900

    Fix methods of Views

--- lib/ver/methods/views.rb
+++ lib/ver/methods/views.rb
@@ -1,20 +1,16 @@
 module VER::Methods
   module Views
     class << self
-      def self.call(widget, command, args)
-        p call: args
-      end
-
-      def change(method, *count)
-        __send__(method, *count)
+      def change(text, action, *count)
+        action.call(text, *count)
       end
 
       def peer(text)
         text.create_peer
       end
 
-      def focus(index = 0)
-        return unless found = layout.views[index - 1]
+      def focus(text, index = 0)
+        return unless found = text.layout.views[index - 1]
         found.push_top
       end
 
@@ -22,91 +18,91 @@ module VER::Methods
         text.view.find_or_create(file)
       end
 
-      def create
-        view.create
+      def create(text)
+        text.view.create
       end
 
-      def close
-        view.close
+      def close(text)
+        text.view.close
       end
 
-      def focus_next
-        view.focus_next
+      def focus_next(text)
+        text.view.focus_next
       end
 
-      def focus_prev
-        view.focus_prev
+      def focus_prev(text)
+        text.view.focus_prev
       end
 
-      def push_up
-        view.push_up
+      def push_up(text)
+        text.view.push_up
       end
 
-      def push_down
-        view.push_down
+      def push_down(text)
+        text.view.push_down
       end
 
-      def push_top
-        view.push_top
+      def push_top(text)
+        text.view.push_top
       end
 
-      def push_bottom
-        view.push_bottom
+      def push_bottom(text)
+        text.view.push_bottom
       end
 
-      def one
-        layout.options.merge! master: 1, stacking: 0
-        layout.apply
+      def one(text)
+        text.layout.options.merge! master: 1, stacking: 0
+        push_top(text)
       end
 
-      def two
-        layout.options.merge! master: 1, stacking: 1
-        layout.apply
+      def two(text)
+        text.layout.options.merge! master: 1, stacking: 1
+        push_top(text)
       end
 
-      def slave_inc
-        stacking = layout.options[:stacking]
-        unless stacking >= layout.views.size
-          layout.options[:stacking] += 1
-          layout.apply
+      def slave_inc(text)
+        stacking = text.layout.options[:stacking]
+        unless stacking >= text.layout.views.size
+          text.layout.options[:stacking] += 1
+          text.layout.apply
         end
       end
 
-      def slave_dec
-        stacking = layout.options[:stacking]
-        layout.options[:stacking] -= 1 if stacking > 0
-        layout.apply
+      def slave_dec(text)
+        stacking = text.layout.options[:stacking]
+        text.layout.options[:stacking] -= 1 if stacking > 0
+        text.layout.apply
       end
 
-      def master_inc
-        layout.options[:master] += 1
-        layout.apply
+      def master_inc(text)
+        text.layout.options[:master] += 1
+        text.layout.apply
       end
 
-      def master_dec
-        master = layout.options[:master]
-        layout.options[:master] -= 1 if master > 0
-        layout.apply
+      def master_dec(text)
+        master = text.layout.options[:master]
+        text.layout.options[:master] -= 1 if master > 0
+        text.layout.apply
       end
 
-      def master_shrink
+      def master_shrink(text)
         center = layout.options[:center]
         layout.options[:center] -= 0.1 if center > 0.1
         layout.apply
       end
 
-      def master_grow
+      def master_grow(text)
         center = layout.options[:center]
         layout.options[:center] += 0.1 if center < 0.9
         layout.apply
       end
 
-      def cycle_next
-        layout.cycle_next(view)
+      def cycle_next(text)
+        text.layout.cycle_next(text.view)
       end
 
-      def cycle_prev
-        layout.cycle_prev(view)
+      def cycle_prev(text)
+        text.layout.cycle_prev(text.view)
       end
     end
   end
