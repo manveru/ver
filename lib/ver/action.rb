934de78762c6aa7f1d7f6e2906cfd181e8da98d8
Michael Fellinger <m.fellinger@gmail.com>
2010-01-25 20:05:01 +0900

    Nicer display of the executed Action

--- lib/ver/action.rb
+++ lib/ver/action.rb
@@ -1,21 +1,30 @@
 module VER
-  class Action < Struct.new(:receiver, :method, :args, :block)
+  class Action < Struct.new(:receiver, :method, :args, :block, :last)
     def initialize(receiver = nil, method = nil, args = [], &block)
       self.receiver = receiver
       self.method = method || :call
       self.args = args
       self.block = block
+      self.last = [self.receiver, self.method, self.args]
     end
 
     def call(widget, *given_args)
       receiver = self.receiver || widget
-      # puts "%p.send(%p, %p, *%p, *%p)" % [receiver, method, widget, args, given_args]
-      receiver.send(method, widget, *args, *given_args)
+      args = [*self.args, *given_args]
+      self.last = [receiver, method, widget.tk_pathname, args]
+      receiver.send(method, widget, *args)
     end
 
     def combine(action)
       new_args = [*args, action].compact
       self.class.new(receiver, method, new_args, &block)
+    end
+
+    def to_s
+      receiver, method, widget, args = last
+      joined = [widget, *args.map{|arg| arg.inspect }].join(' ')
+
+      "%s.%s(%s)" % [receiver, method, joined]
     end
   end
 end
