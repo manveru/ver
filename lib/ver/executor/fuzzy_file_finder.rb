7871e5fe8ee3dcc75f8ad3590d1d50ed75dedf6b
Michael Fellinger <m.fellinger@gmail.com>
2010-02-05 15:38:10 +0900

    Adjust score for ExFFF

--- lib/ver/executor/fuzzy_file_finder.rb
+++ lib/ver/executor/fuzzy_file_finder.rb
@@ -68,9 +68,9 @@ module VER
         values.map do |value|
           score = value.last
           tag =
-            if score < 0.3; :icy
-            elsif score < 0.6; :cold
-            elsif score < 0.9; :warm
+            if score < 0.25; :icy
+            elsif score < 0.5; :cold
+            elsif score < 0.75; :warm
             else; :hot
             end
 
