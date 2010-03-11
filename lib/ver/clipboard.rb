b30c78f33de99208306cd60f9e114230030b5739
Michael Fellinger <m.fellinger@gmail.com>
2010-03-11 20:27:52 +0900

    Major improvments to VER::Clipboard

--- lib/ver/clipboard.rb
+++ lib/ver/clipboard.rb
@@ -1,29 +1,94 @@
 module VER
+  # Tries to provide a simple interface to the X clipboard.
+  # In particular we worry about exceptions and encodings.
+  #
+  # Please note that you should not use this module if you want to set or get
+  # binary contents from the clipboard, you'll have to use {Tk::Clipboard}
+  # directly or put up with a base64 and marshal overhead.
   module Clipboard
     module_function
 
-    def get(type = 'UTF8_STRING')
-      case type
-      when 'STRING'
-        Tk::Clipboard.get(VER.root, type).encode(Encoding::ASCII)
-      when 'UTF8_STRING'
-        got = Tk::Clipboard.get(VER.root, type)
-        got.encode!(Encoding::ASCII) if got.ascii_only?
-        got
+    def get(type)
+      Tk::Clipboard.get(VER.root, type)
+    end
+
+    def dwim
+      string || marshal
+    end
+
+    def string
+      got = Tk.execute('::tk::GetSelection', VER.root, 'CLIPBOARD').to_s
+    rescue => ex
+      l ex
+      nil
+    else
+      if got.ascii_only?
+        got.encode!('ASCII')
       else
-        Tk::Clipboard.get(VER.root, type)
+        got.force_encoding('UTF-8')
+        got.encode!(Encoding.default_external)
+      end
+
+      got
+    end
+
+    def marshal
+      got = get('RUBY_MARSHAL')
+    rescue
+      nil
+    else
+      Marshal.load(got.unpack('m0').first)
+    end
+
+    def set(string, type)
+      Tk::Clipboard.set(string, type: type)
+    end
+
+    # dwim stands for "do what i mean", and for most of your clipboard needs
+    # this should just do what you want.
+    def dwim=(object)
+      if object.respond_to?(:to_str)
+        self.string = object.to_str
+      else
+        self.marshal = object
       end
     end
 
-    def set(string)
+    def string=(string)
       case string.encoding
-      when Encoding::UTF_8
-        Tk::Clipboard.set(string, type: 'UTF8_STRING')
       when Encoding::ASCII
-        Tk::Clipboard.set(string, type: 'STRING')
+        self.ascii = string
+      when Encoding::UTF_8
+        self.utf8 = string
       else
-        raise ArgumentError, "Encoding mismatch, argument is %p" % [string.encoding]
+        self.utf8 = string.encode('UTF-8')
       end
+    end
+
+    # Note that this will encode string without making a copy, so only pass
+    # UTF-8 strings or make a copy if you mind the encoding change.
+    #
+    # Use this method on *nix if possible, most modern applications query for
+    # this type first.
+    def utf8=(string)
+      set(string.encode!('UTF-8'), 'UTF8_STRING')
+    end
+
+    # Note that this will encode string without making a copy, so only pass
+    # US-ASCII strings or make a copy if you mind the encoding change.
+    #
+    # The benefit of using this method is that it should work with older
+    # applications too, as they might not know about UTF8_STRING.
+    def ascii=(string)
+      set(string.encode!('US-ASCII'), 'STRING')
+    end
+
+    # This will put any object capable of being marshalled into the clipboard.
+    # We use RUBY_MARSHAL as type, but it's also base64-encoded as specified in
+    # RFC 4648 to keep it ASCII.
+    def marshal=(object)
+      marshal = [Marshal.dump(object)].pack('m0')
+      set(marshal, 'RUBY_MARSHAL')
     end
   end
 end
--- spec/ver/clipboard.rb
+++ spec/ver/clipboard.rb
@@ -6,50 +6,60 @@ require_relative '../helper'
 # Have to read the ICCCM and about their conventions first.
 # Obviously, Marshal would be very convenient, but I doubt it's best practice.
 
+shared :clipboard_spec do
+  before do
+    Tk::Clipboard.clear
+  end
+end
+
 VER.spec do
   describe 'VER::Clipboard' do
-    describe 'set' do
-      before do
-        Tk::Clipboard.clear
-      end
+    describe 'dwim=' do
+      behaves_like :clipboard_spec
 
       it 'copies an ASCII String as STRING' do
         string = "some string".encode(Encoding::ASCII)
-        VER::Clipboard.set(string)
+        VER::Clipboard.dwim = string
         Tk::Clipboard.get(VER.root, 'STRING').should == string
       end
 
       it "copies an ASCII String as UTF8_STRING" do
         string = "some string".encode(Encoding::ASCII)
-        VER::Clipboard.set(string)
+        VER::Clipboard.dwim = string
         Tk::Clipboard.get(VER.root, 'UTF8_STRING').should == string
       end
 
       it 'copies an UTF-8 String as UTF8_STRING' do
         string = "日本語".encode(Encoding::UTF_8)
-        VER::Clipboard.set(string)
+        VER::Clipboard.dwim = string
         Tk::Clipboard.get(VER.root, 'UTF8_STRING').should == string
       end
 
       it "doesn't copy an UTF-8 String as STRING" do
         string = "日本語".encode(Encoding::UTF_8)
-        VER::Clipboard.set(string)
+        VER::Clipboard.dwim = string
         lambda{
           Tk::Clipboard.get(VER.root, 'STRING')
         }.should.raise(RuntimeError).message.
          should.start_with(%(CLIPBOARD selection doesn't exist or form "STRING" not defined))
       end
+
+      it 'puts an object into the clipboard' do
+        obj = ["hi there", (24..42), :Symbol, Class]
+        VER::Clipboard.dwim = obj
+        got = Tk::Clipboard.get(VER.root, 'RUBY_MARSHAL')
+        got_obj = Marshal.load(got.unpack('m0').first)
+        got_obj.should == obj
+      end
     end
 
-    describe 'get' do
-      before do
-        Tk::Clipboard.clear
-      end
+    describe 'dwim' do
+      behaves_like :clipboard_spec
 
       it 'gets an ASCII String from STRING' do
         string = "some string".encode(Encoding::ASCII)
         Tk::Clipboard.set(string, type: 'STRING')
-        got = VER::Clipboard.get('STRING')
+        got = VER::Clipboard.dwim
         got.should == string
         got.encoding.should == Encoding::ASCII
       end
@@ -57,7 +67,7 @@ VER.spec do
       it "gets an ASCII String from ASCII in UTF8_STRING" do
         string = "some string".encode(Encoding::ASCII)
         Tk::Clipboard.set(string, type: 'UTF8_STRING')
-        got = VER::Clipboard.get('UTF8_STRING')
+        got = VER::Clipboard.dwim
         got.should == string
         got.encoding.should == Encoding::ASCII
       end
@@ -65,9 +75,17 @@ VER.spec do
       it 'gets an UTF-8 String from UTF-8 in UTF8_STRING' do
         string = "日本語".encode(Encoding::UTF_8)
         Tk::Clipboard.set(string, type: 'UTF8_STRING')
-        got = VER::Clipboard.get('UTF8_STRING')
+        got = VER::Clipboard.dwim
         got.should == string
         got.encoding.should == Encoding::UTF_8
+      end
+
+      it 'puts an object into the clipboard' do
+        obj = ["hi there", (24..42), :Symbol, Class]
+        marshal = [Marshal.dump(obj)].pack('m0')
+        Tk::Clipboard.set(marshal, type: 'RUBY_MARSHAL')
+        got = VER::Clipboard.dwim
+        got.should == obj
       end
     end
   end
