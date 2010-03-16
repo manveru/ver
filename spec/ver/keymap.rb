afc4589f33b89fc3e1109c9c2912ec28607d4812
Michael Fellinger <m.fellinger@gmail.com>
2010-03-17 00:39:07 +0900

    Simplify keymap spec

--- spec/ver/keymap.rb
+++ spec/ver/keymap.rb
@@ -4,75 +4,76 @@ VER.spec do
   describe VER::Keymap do
     Keymap = VER::Keymap
 
+    before do
+      @keymap = Keymap.new
+    end
+
+    def keymap
+      @keymap
+    end
+
     it 'maps objects to a key' do
-      km = Keymap.new
-      km['t'] = 'our t'
-      km['t'].should == 'our t'
+      keymap['t'] = 'our t'
+      keymap['t'].should == 'our t'
     end
 
     it 'maps objects to a sequence of keys' do
-      km = Keymap.new
-      km['d', 'd'] = 'our d'
-      km['d', 'd'].should == 'our d'
+      keymap['d', 'd'] = 'our d'
+      keymap['d', 'd'].should == 'our d'
     end
 
     it 'creates deeper nestings' do
-      km = Keymap.new
-      km['a', 'b', 'c', 'd'] = :foo
-      km['a', 'b', 'c', 'd'].should == :foo
+      keymap['a', 'b', 'c', 'd'] = :foo
+      keymap['a', 'b', 'c', 'd'].should == :foo
     end
 
     it 'branches as needed' do
-      km = Keymap.new
-      km['a', 'b', 'c', 'd'] = 'abcd'
-      km['a', 'b', 'c', 'e'] = 'abce'
-      km['a', 'b', 'c', 'd'].should == 'abcd'
-      km['a', 'b', 'c', 'e'].should == 'abce'
+      keymap['a', 'b', 'c', 'd'] = 'abcd'
+      keymap['a', 'b', 'c', 'e'] = 'abce'
+      keymap['a', 'b', 'c', 'd'].should == 'abcd'
+      keymap['a', 'b', 'c', 'e'].should == 'abce'
     end
 
     it "indicates when the given key-sequence doesn't match yet" do
-      km = Keymap.new
-      km['a', 'b', 'c', 'd'] = 'abcd'
-      km['a', 'b'].should.be.kind_of Keymap::Results::Incomplete
+      keymap['a', 'b', 'c', 'd'] = 'abcd'
+      keymap['a', 'b'].should.be.kind_of Keymap::Results::Incomplete
     end
 
     it "indicates when the given key-sequence will never match" do
-      km = Keymap.new
-      km['a', 'b', 'c', 'd'] = 'abcd'
-      km['x', 'y'].should.be.kind_of Keymap::Results::Impossible
+      keymap['a', 'b', 'c', 'd'] = 'abcd'
+      keymap['x', 'y'].should.be.kind_of Keymap::Results::Impossible
     end
 
     it 'performs a merge with another keymap' do
-      kma = Keymap.new
-      kma['a'] = 'kma a'
+      keymap1 = Keymap.new
+      keymap1['a'] = 'keymap1 a'
 
-      kmb = Keymap.new
-      kmb['b'] = 'kmb b'
+      keymap2 = Keymap.new
+      keymap2['b'] = 'keymap2 b'
 
-      kmab = kma.merge(kmb)
-      kmab['a'].should == 'kma a'
-      kmab['b'].should == 'kmb b'
+      keymap1b = keymap1.merge(keymap2)
+      keymap1b['a'].should == 'keymap1 a'
+      keymap1b['b'].should == 'keymap2 b'
     end
 
     it "doesn't overwrite existing sequences on merge" do
-      kma = Keymap.new
-      kma['a'] = 'kma a'
+      keymap1 = Keymap.new
+      keymap1['a'] = 'keymap1 a'
 
-      kmb = Keymap.new
-      kmb['a'] = 'kmb a'
-      kmb['b'] = 'kmb b'
+      keymap2 = Keymap.new
+      keymap2['a'] = 'keymap2 a'
+      keymap2['b'] = 'keymap2 b'
 
-      kmab = kma.merge(kmb)
-      kmab['a'].should == 'kma a'
-      kmab['b'].should == 'kmb b'
+      keymap1b = keymap1.merge(keymap2)
+      keymap1b['a'].should == 'keymap1 a'
+      keymap1b['b'].should == 'keymap2 b'
     end
 
     it 'overrides existing sequences on assignment' do
-      km = Keymap.new
-      km['a'] = 'km a1'
-      km['a'].should == 'km a1'
-      km['a'] = 'km a2'
-      km['a'].should == 'km a2'
+      keymap['a'] = 'keymap a1'
+      keymap['a'].should == 'keymap a1'
+      keymap['a'] = 'keymap a2'
+      keymap['a'].should == 'keymap a2'
     end
   end
 end
